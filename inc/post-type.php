35686d8856d11797182c045cc356fddec8efb7a2
VVolkov833 <vadim.volkov.dev@gmail.com>
2024-07-04 17:23:23 +0200

    add marking plugins controlled git-branch-follow

--- inc/post-type.php
+++ inc/post-type.php
@@ -70,5 +70,38 @@ add_filter( 'plugin_action_links_'.FCGBF_BSN, function($links) {
     return $links;
 });
 
+// mark and link managed plugins in the plugins' list
+add_filter( 'plugin_action_links', function($actions, $plugin_file, $plugin_data, $context) {
 
-// the main list view change
+    $plugins_to_mark = list_rep_posts();
+
+    foreach ( $plugins_to_mark as $plugin_post ) {
+        if ( strpos( $plugin_file, $plugin_post->post_title.'/' ) !== 0 ) { continue; }
+        add_filter( 'plugin_action_links_'.$plugin_file, function($links) use ($plugin_post) {
+            $rep_link = '<a
+                href="' . esc_url( admin_url( 'post.php?action=edit&post='.$plugin_post->ID ) ) . '"
+                class="wp-menu-image dashicons-before dashicons-open-folder"
+                title="The updates are followed this Git repository"
+            ></a>';
+            array_unshift( $links, $rep_link );
+            return $links;
+        });
+    }
+
+    return $actions;
+}, 10, 4 );
+
+
+function list_rep_posts() {
+    global $wpdb;
+
+    static $plugins_to_mark = null;
+    if ( $plugins_to_mark !== null ) { return $plugins_to_mark; }
+
+    $plugins_to_mark = $wpdb->get_results( $wpdb->prepare( "
+        SELECT ID, post_title FROM $wpdb->posts WHERE post_type = %s AND post_status != 'trash'
+        ",
+        FCGBF_SLUG
+    ));
+    return $plugins_to_mark;
+}
